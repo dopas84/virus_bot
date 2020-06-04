94ffd0044e36471c8fec31d0a70d5494f208b29d
dopas84 <66274144+dopas84@users.noreply.github.com>
2020-06-04 19:24:11 +0700

    VictimWA.php

--- virusbot.php
+++ virusbot.php
@@ -2,7 +2,7 @@
  \ \   / (_)_ __ _   _ ___    
   \ \ / /| | '__| | | / __|     
    \ V / | | |  | |_| \__ \      
-    \_/  |_|_|   \__,_|___/   (By Prince Yasin)
+    \_/  |_|_|   \__,_|___/   (By Prince Dopas)
 
 Copy full text and send to victim
 
